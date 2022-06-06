536d7fa5d33ee418cbd7fd34f0709c78cd32c00a
Mitterand Ekole <mittrandekole@gmail.com>
2022-06-06 22:31:30 +0100

    add scripts to get all 68 landmark coordinates

--- src/inference.py
+++ src/inference.py
@@ -11,9 +11,8 @@ import time
 plt.ion()
 import dlib
 from network import *
+import numpy as np
 
-
-#config='D:/Dev Projects/DeepStack/face-landmark/config/300w.yaml'
 def load_model(model_path):
   model=XceptionNet()
   model.load_state_dict(torch.load(model_path, map_location='cpu'))
@@ -54,16 +53,20 @@ detector=dlib.get_frontal_face_detector()
 
 def inference(frame):
   gray=cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+  
   faces=detector(gray,1)
 
   outputs=[]
 
+  
   for (i, face) in enumerate(faces):
     (x, y, w, h) = face_utils.rect_to_bb(face)
     crop_img = gray[y: y + h, x: x + w]
     
     transformed_img= transform_img(crop_img)
+
     landmarks_predictions = model(transformed_img.cpu())
+
     outputs.append((landmarks_predictions.cpu(), (x, y, h, w)))
   return landmarks_draw(frame, outputs)
 
@@ -102,40 +105,38 @@ def output_video(video, name, seconds = None):
 
 ''' inference on image'''
 
-
 def image_output(image, name):
   print("Analysing image")
-  #frame=cv2.imread(image)
-  start_time=time.time()
-
+ 
 
   outputs=[]
 
-  #writer=cv2.imwrite('img.jpg', image)
+
 
   output=inference(image)
   outputs.append(output)
 
-  #writer=(cv2.cvtColor(output,cv2.COLOR_RGB2GRAY))
-  writer=output
-  disp_img=cv2.imwrite('img.jpg',writer)
+  vec=np.empty((68,2), dtype=int)
+  for b in range(68):
+    vec[b][0]=output[0][b][0]
+    vec[b][1]=output[0][b][1]
 
-  end_time=time.time()
+  print(vec)
+  
+  disp_img=cv2.imwrite(name + '.jpg', output)
 
-  process_time=end_time-start_time
-
-  print("Inference time: {:.2f} seconds".format(process_time))
 
   #writer.release()
 
-  return outputs
-
-''' runing inference on image'''
-
-img=cv2.imread('D:/Dev Projects/DeepStack/proto.facelandmarkdetector/data/faces/299733036_fff5ea6f8e.jpg')
-outputs=image_output(img,'D:/Dev Projects/DeepStack/proto.facelandmarkdetector/demo')
-
-#plt.figure(figsize=(11,11))
-#plt.imshow(outputs[0])
+  
+  return outputs, disp_img
 
 
+
+  
+
+
+
+img_path='D:/Dev Projects/DeepStack/proto.facelandmarkdetector/data/faces/144044282_87cf3ff76e.jpg'
+img=cv2.imread(img_path)
+outputs=image_output(img,'D:/Dev Projects/DeepStack/proto.facelandmarkdetector/demo/img_output')
